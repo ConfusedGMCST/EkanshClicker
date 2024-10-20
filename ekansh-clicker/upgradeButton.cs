c4325ebf18d9fccf7cc958a4661b20b014614620
ConfusedG <christian.haase@mcvts.org>
2024-10-20 12:35:55 -0400

    broken math
    im gonna work on this another day lol

--- ekansh-clicker/upgradeButton.cs
+++ ekansh-clicker/upgradeButton.cs
@@ -11,15 +11,19 @@ public partial class upgradeButton : Area2D
 	public string price = "price";
 	public float currentPrice;
     public float EkanshClicks;
+	public float currentCps;
 	public float priceModifier;
+	public float currentCpsModifier;
+	public float anotherCpsFloat;
     public bool onButton = false;
+	public float fpsCounter = 0;
+	public float purchasedUpgrades;
 	public override void _Ready()
 	{
         string node2D = "/root/Node2D/";
         button = GetNode<Area2D>(node2D + "upgradeButton1");
 		gameNode = GetNode<Node2D>(node2D);
 		EkanshClicks = (float) gameNode.GetMeta("EkanshClicks");
-
     }
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -28,6 +32,10 @@ public partial class upgradeButton : Area2D
         EkanshClicks = (float) gameNode.GetMeta("EkanshClicks");
 		currentPrice = (float) button.GetMeta(price);
 		priceModifier = (float) gameNode.GetMeta("priceModifier");
+		currentCps = (float) gameNode.GetMeta(totalCps);
+		currentCpsModifier = (float) button.GetMeta(cpsAdd);
+		anotherCpsFloat = (float) button.GetMeta(addedCps);
+        purchasedUpgrades = (float) button.GetMeta("purchasedUpgrades");
 
         if (Input.IsActionJustPressed("click") && onButton == true && EkanshClicks >= currentPrice) 
 		{
@@ -35,7 +43,14 @@ public partial class upgradeButton : Area2D
 			EkanshClicks -= currentPrice;
             button.SetMeta(price, currentPrice *= priceModifier);
             gameNode.SetMeta("EkanshClicks", EkanshClicks);
+			button.SetMeta(addedCps, anotherCpsFloat += currentCpsModifier);
+			button.SetMeta("purchasedUpgrades", purchasedUpgrades++);
             GD.Print("Ekansh Clicks: " + gameNode.GetMeta("EkanshClicks") + "\nUpgrade Price: " + button.GetMeta(price));
+		}
+		fpsCounter++;
+		if (fpsCounter % 60 == 0) 
+		{
+			gameNode.SetMeta(totalCps, (float) gameNode.GetMeta("EkanshClicks") + currentCpsModifier * purchasedUpgrades);		
 		}
 	}
 
